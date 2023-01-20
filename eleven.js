d1850970b4d2b6d4371c010190db77b272332a71
Dharaneesh2248 <dharaneesh2248@gmail.com>
2023-01-20 10:12:33 +0530

    file

---
+++ eleven.js
@@ -0,0 +1,7 @@
+function add()
+{
+   let a=document.getElementById("a").value;
+   let b=document.getElementById("b").value;
+   let c=Number(a)+Number(b);
+   document.getElementById("c").value=c;
+}
